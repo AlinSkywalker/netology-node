c3e7cbaaafb6dbc40623e952282a20d45ea15701
Alina Shevchenko <alinskywalker@gmail.com>
2025-05-12 16:31:01 +0300

    homework 1.5

---
+++ 1.5/weather.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+const http = require('http')
+
+const argv = yargs(hideBin(process.argv))
+    .option('city', {
+        alias: 'c',
+        type: 'string',
+        description: 'город'
+    })
+    .argv
+
+console.log(argv)
+const myAPIKey = process.env.WEATHER_API_KEY
+
+const { city } = argv
+if (city) {
+    const url = `http://api.weatherstack.com/current?access_key=${myAPIKey}&query=${city}`
+    console.log('url', url)
+    http.get(url, (res) => {
+        const { statusCode } = res
+        if (statusCode !== 200) {
+            console.log(`statusCode: ${statusCode}`)
+            return
+        }
+
+        res.setEncoding('utf8')
+        let rowData = ''
+        res.on('data', (chunk) => rowData += chunk)
+        res.on('end', () => {
+            let parseData = JSON.parse(rowData)
+            console.log(`Текущая температура: ${parseData.current.temperature}`)
+        })
+    }).on('error', (err) => {
+        console.error(err)
+    })
+}
+else {
+    console.error('Не указан город')
+}
